348bda82ff0c1f1e55e7065ec153e820efb2c136
nk21t5c7 <nodokahanaki@gmail.com>
2025-02-09 22:37:08 -0800

    Add material ui

--- weather/vite-project/src/Components/Form.tsx
+++ weather/vite-project/src/Components/Form.tsx
@@ -1,21 +1,58 @@
+import {useState} from 'react';
+import { FormControl, InputLabel, Input, TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+
+interface formData{
+    city: string;
+    country:string;
+    description: string;
+    latitude: number;
+    longitude: number;
+}
+
 const Form = () => {
+    const [city, setCity] = useState<string>("");
+    const [country, setCountry] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [lat, setLat] = useState<number>();
+    const [lon, setLon] = useState<number>();
+
   return (
-    <div className="m-auto max-w-[1200px] p-6">
-      <h3 className="text-2xl">Add another city</h3>
-      <form className="">
-        <label htmlFor="cityName">City</label>
-        <input className = "border-b-blue-950 border-1" id="cityName" type="text"></input>
+    <div className="m-auto max-w-[1200px]  p-6 ">
+      <h3 className="text-2xl my-4">Add another city</h3>
+      <form className="grid grid-cols-[1fr_1fr] gap-4">
+        <InputLabel htmlFor = "city" className="col-start-1 col-end-2">City</InputLabel>
+        <Input id='city' name = "city" className="col-start-2 col-end-3"/>
 
-        <label htmlFor="countryName">Country</label>
-        <input className = "border-b-blue-950 border-1" id="countryName" type="text"></input>
+        <InputLabel htmlFor = "country" className="col-start-1 col-end-2">Country</InputLabel>
+        <Input id='country'  name = "country" className="col-start-2 col-end-3"/>
 
-        <label htmlFor="description">Description</label>
-        <textarea className = "border-b-blue-950 border-1" id="description"></textarea>
+        <InputLabel htmlFor = "latitude" className="col-start-1 col-end-2">Latitude</InputLabel>
+        <Input id='latitude' name = "latitude" className="col-start-2 col-end-3"/>
 
-        <input className = "border-b-blue-950 border-1" type= "file" />
+        <InputLabel htmlFor = "longitude" className="col-start-1 col-end-2">Longitude</InputLabel>
+        <Input id='longitude' name='longitude' className="col-start-2 col-end-3"/>
 
+        <Button
+          component="label"
+          role={undefined}
+          variant="outlined"
+          tabIndex={-1}
+          className="col-start-1 col-end-3 py-4"
+          //   startIcon={<CloudUploadIcon />}
+        >
+          <input type="file" className="opacity-0 appearance-none absolute" />
+          Upload files
+        </Button>
 
-        <input value="Submit" className = "border-1" type="submit" />
+        <Button
+          variant="contained"
+          className="border-1 col-start-1 col-end-3 mt-10 py-2  justify-center self-center content-center"
+          type="submit"
+        >
+          <input type="submit" className="opacity-0 appearance-none absolute" />
+          Submit
+        </Button>
       </form>
     </div>
   );
