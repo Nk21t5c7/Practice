bcb0e79592823ffccd695a1b681ff013e145c95d
nk21t5c7 <nodokahanaki@gmail.com>
2025-02-27 23:54:22 -0800

    routing practice project

---
+++ routing-next/src/app/page.tsx
@@ -0,0 +1,15 @@
+
+import Header from "@/components/Header";
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <Header />
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        
+      </footer>
+    </div>
+  );
+}
