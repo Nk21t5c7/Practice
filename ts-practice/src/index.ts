3b28a98cff7394e5446267b58e9548054cfa9345
nk21t5c7 <nodokahanaki@gmail.com>
2025-03-12 23:55:45 -0700

    Add resolver

--- ts-practice/src/index.ts
+++ ts-practice/src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import type { Express, Request, Response } from "express";
+import type { Application, Request, Response } from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import { typeDefs } from "./schema";
 import path from "path";
-const app: Express = express();
-const port = 3055;
+import { resolver } from "./resolver";
+const app: Application = express();
+const port: number = 3055;
 
 app.use(express.static("public"));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: resolver,
+});
+
+// server.applyMiddleware({ app });
 
 app.get("/about", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../public", "pages", "about.html"));
@@ -14,4 +24,10 @@ app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(port, () => console.log(`App working on ${port}`));
+// app.listen(port, () => console.log(`App working on ${port}`));
+(async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen(port, () => console.log(`App working on ${port}`));
+})();
--- ts-practice/src/resolver.ts
+++ ts-practice/src/resolver.ts
@@ -1,20 +1,31 @@
-import { ApolloServer } from "apollo-server";
 import { readFileSync } from "fs";
 import path from "path";
-import { typeDefs } from "./schema";
 
-const data = JSON.parse(readFileSync(path.join(__dirname, "data.json"), "utf-8"));
+const data = JSON.parse(
+  readFileSync(path.join(__dirname, "data.json"), "utf-8")
+);
 
+type Dog = {
+  breed: String;
+  name: String;
+  age: Number;
+  isNaughty: Boolean;
+};
 
-// if theres multiple schema => resolver for each schema
+// multiple schema => resolver for each schema
 const greetingResolver = {
   Query: {
     hello: () => "Hello",
   },
 };
 
-const dogSchema = {
-    Query: {
-        dog:(_, {})
-    }
+const dogResolver = {
+  Query: {
+    dog: (_: unknown, { breed }: { breed: string }) =>
+      data.dogs.find((dog: Dog) => dog.breed === breed),
+  },
+};
+export const resolver = {
+    ...greetingResolver,
+    ...dogResolver
 }
