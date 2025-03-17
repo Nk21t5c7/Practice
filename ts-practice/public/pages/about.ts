91dcc2e20946c4903fca7a2a74560d06978d7288
nk21t5c7 <nodokahanaki@gmail.com>
2025-03-16 21:35:19 -0700

    Add code to display data

---
+++ ts-practice/public/pages/about.ts
@@ -0,0 +1,34 @@
+import Axios from "axios";
+
+type Dog = {
+  breed: string;
+  name: string;
+  age: number;
+  isNaughty: boolean;
+};
+
+alert('dog');
+const getData = () => {
+  document.getElementById("dogList")!.innerHTML = "";
+  Axios.get("/about")
+    .then((result) => {
+      console.log(result);
+      const data = result.data.dogs;
+      data.forEach((e: Dog) => {
+        const list = document.createElement("li");
+        const { breed, name, age, isNaughty }: Dog = e;
+        list.innerText = `${name} - ${breed}, ${age}years old`;
+        if (isNaughty) {
+          list.innerHTML += '<i class="fa-solid fa-skull"></i>';
+        }
+
+        // ! = not null/undefined
+        document.getElementById("dogList")!.append(list);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+getData();
