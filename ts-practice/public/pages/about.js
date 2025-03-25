da3e17f3e106678db55f0976793bce6ed1a18755
nk21t5c7 <nodokahanaki@gmail.com>
2025-03-24 23:06:40 -0700

    Fix about html and js to

--- ts-practice/public/pages/about.js
+++ ts-practice/public/pages/about.js
@@ -1,26 +1,26 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = require("axios");
-alert('dog');
-var getData = function () {
+
+export const getData = () => {
+    alert("dog");
     document.getElementById("dogList").innerHTML = "";
-    axios_1.default.get("/api/about")
-        .then(function (result) {
+    axios.get("http://localhost:3055/api/about")
+      .then((result) => {
         console.log(result);
-        var data = result.data.dogs;
-        data.forEach(function (e) {
-            var list = document.createElement("li");
-            var breed = e.breed, name = e.name, age = e.age, isNaughty = e.isNaughty;
-            list.innerText = "".concat(name, " - ").concat(breed, ", ").concat(age, "years old");
-            if (isNaughty) {
-                list.innerHTML += '<i class="fa-solid fa-skull"></i>';
-            }
-            // ! = not null/undefined
-            document.getElementById("dogList").append(list);
+        const data = result.data.dogs;
+        data.forEach(e => {
+          const list = document.createElement("li");
+          const { breed, name, age, isNaughty } = e;
+          list.innerText = `${name} - ${breed}, ${age}years old`;
+          if (isNaughty) {
+            list.innerHTML += '<i class="fa-solid fa-skull"></i>';
+          }
+  
+          // ! = not null/undefined
+          document.getElementById("dogList").append(list);
         });
-    })
-        .catch(function (err) {
+      })
+      .catch((err) => {
         console.log(err);
-    });
-};
-getData();
+      });
+  };
+  
+  getData();
