b763a334ab1881bf877e78dfc479176cdc2ff28d
nk21t5c7 <nodokahanaki@gmail.com>
2025-04-02 23:55:04 -0700

    Add dataset page

---
+++ animals/src/pages/dataset.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Header } from "../components/Header";
+
+const Dataset = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2 className="text-red-200">Lorem, ipsum dolor.</h2>
+        
+
+      </main>
+    </div>
+  );
+};
+
+export default Dataset;
