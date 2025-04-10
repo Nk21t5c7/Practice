c03b4b1f9973ce6f9e80d92e348a46eaea415e4f
nk21t5c7 <nodokahanaki@gmail.com>
2025-04-09 23:58:28 -0700

    add mongoose and apolloserver

--- animals/src/components/Header.tsx
+++ animals/src/components/Header.tsx
@@ -3,17 +3,15 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className="">
       <header className="p-4 flex items-center justify-between ">
         <h1 className="font-bold text-3xl uppercase tracking-wide"><Link href="/">animals</Link></h1>
         <nav>
-          <ul>
+          <ul className="list-none">
             <li>
               <Link href="/dataset">Data</Link>
             </li>
           </ul>
         </nav>
       </header>
-    </div>
   );
 };
