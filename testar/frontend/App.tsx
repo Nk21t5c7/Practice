a4b56ec5606d398d5aef625c817f5e8683fbe977
nk21t5c7 <nodokahanaki@gmail.com>
2025-05-20 18:20:31 -0700

    Remove unnecessary files

--- testar/frontend/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ViroARSceneNavigator } from '@viro-community/react-viro';
-import MyScene from './src/components/MyScene';
-
-export default function App() {
-  return <ViroARSceneNavigator initialScene={{scene: MyScene}} />;
-}
