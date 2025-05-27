970dfbf199941d1a710bd620c26e47ff62a715e2
nk21t5c7 <nodokahanaki@gmail.com>
2025-05-26 23:58:32 -0700

    Feat: Add error handling

--- remove-background/backend/python/removeBg.py
+++ remove-background/backend/python/removeBg.py
@@ -1,11 +1,18 @@
 from rembg import remove
 import sys
+import traceback
 
 input_path = sys.argv[1]
 output_path = sys.argv[2]
 
-with open(input_path, "rb") as i:
-    with open(output_path, "wb") as o:
-        input = i.read()
-        output = remove(input)
-        o.write(output)
+try: 
+    print('1')
+    with open(input_path, "rb") as i:
+        print(input_path)
+        with open(output_path, "wb") as o:
+            input = i.read()
+            output = remove(input)
+            o.write(output)
+except Exception as e :
+    # print(e)
+    traceback.print_exc()
